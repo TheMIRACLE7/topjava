a941195099166197cbc7125a9794bf3287e89dd6
Sergey <s.s.miroshnichenko@mail.ru>
2016-04-07 11:21:52 +0400

    Failed to load ApplicationContext

---
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/ProxyUserMealRepository.java
@@ -0,0 +1,38 @@
+package ru.javawebinar.topjava.repository.datajpa;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.model.UserMeal;
+
+import java.util.List;
+
+/**
+ * GKislin
+ * 27.03.2015.
+ */
+@Transactional(readOnly = true)
+public interface ProxyUserMealRepository extends JpaRepository<UserMeal, Integer> {
+
+    @Transactional
+    @Modifying
+//    @Query(name = User.DELETE)
+    @Query("DELETE FROM UserMeal m WHERE m.id=:id AND m.user.id=:userId")
+    int delete(@Param("id") int id, @Param("userId") int userId);
+
+    @Override
+    @Transactional
+    UserMeal save(UserMeal meal);
+
+    @Override
+    UserMeal findOne(Integer id);
+
+    @Override
+    List<UserMeal> findAll(Sort sort);
+
+    User getByEmail(String email);
+}
